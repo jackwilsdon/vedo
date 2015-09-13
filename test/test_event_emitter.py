6820967fb5a63a49755d89efb9887022a07b7880
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-09-13 02:12:54 +0100

    Update ValueHolder to use a lock value

--- test/test_event_emitter.py
+++ test/test_event_emitter.py
@@ -6,27 +6,35 @@ from vedo.event_emitter import EventEmitter
 
 
 class ValueHolder(object):
-    def __init__(self, value=False, one_time=True):
+    def __init__(self, value=None, lock_on_value=False, lock_value=None):
         self._value = value
-        self._one_time = one_time
+        self._lock_on_value = lock_on_value
+        self._lock_value = lock_value
 
-        self._already_set = False
+        self._locked = False
 
     @property
     def value(self):
         return self._value
 
     @property
-    def one_time(self):
-        return self._one_time
+    def lock_on_value(self):
+        return self._lock_on_value
 
     @property
-    def already_set(self):
-        return self._already_set
+    def lock_value(self):
+        return self._lock_value
+
+    @property
+    def locked(self):
+        return self._locked
 
     @value.setter
     def value(self, value):
-        if self.one_time and not self.already_set:
+        if not self.locked:
+            if self.lock_on_value and value == self.lock_value:
+                self._locked = True
+
             self._value = value
 
     def set(self, value):
