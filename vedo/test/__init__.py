7cceb382bd6c150c3a862a8e2dad409191718888
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-09-14 20:02:52 +0100

    Remove class test case import

--- vedo/test/__init__.py
+++ vedo/test/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 
-from vedo.test.class_test_case import ClassTestCase
 from vedo.test.output_recorder import OutputRecorder
 from vedo.test.value_container import ValueContainer
 
-__all__ = ['ClassTestCase', 'OutputRecorder', 'ValueContainer']
+__all__ = ['OutputRecorder', 'ValueContainer']
