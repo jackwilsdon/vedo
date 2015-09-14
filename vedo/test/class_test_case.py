855627368375c038e030342e61707d22f3054c8e
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-09-14 17:30:50 +0100

    Remove support for positional args (too messy and unreliable)

--- vedo/test/class_test_case.py
+++ vedo/test/class_test_case.py
@@ -3,30 +3,14 @@ from __future__ import absolute_import
 from unittest import TestCase
 from nose.tools import nottest
 
-def merge_lists(base, cover, skip=object()):
-    merged = [element for element in base]
-    max_length = max(len(merged), len(cover))
 
-    for index, cover_value in enumerate(cover):
-        if cover_value == skip:
-            continue
-        if index >= len(merged):
-            merged.append(cover[index])
-        else:
-            merged[index] = cover[index]
-
-    return merged
-
-
-def merge_dicts(base, cover, skip=object()):
+def merge_dicts(base, cover):
     merged = {}
 
     for key in base:
         merged[key] = base[key]
 
     for key in cover:
-        if cover[key] == skip:
-            continue
         merged[key] = cover[key]
 
     return merged
@@ -38,17 +22,8 @@ class ClassTestCase(TestCase):
     def __init__(self, *args, **kwargs):
         super(ClassTestCase, self).__init__(*args, **kwargs)
 
-        self._args = []
         self._kwargs = {}
         self._test_class = None
-
-    @property
-    def args(self):
-        return self._args
-
-    @args.setter
-    def args(self, args):
-        self._args = args
 
     @property
     def kwargs(self):
@@ -66,11 +41,10 @@ class ClassTestCase(TestCase):
     def test_class(self, test_class):
         self._test_class = test_class
 
-    def create_instance(self, *args, **kwargs):
-        merged_args = merge_lists(self.args, args, self.DEFAULT)
-        merged_kwargs = merge_dicts(self.kwargs, kwargs, self.DEFAULT)
-
+    def create_instance(self, **kwargs):
         if self.test_class is None:
             return None
 
-        return self.test_class(*merged_args, **merged_kwargs)
+        merged_kwargs = merge_dicts(self.kwargs, kwargs)
+
+        return self.test_class(**merged_kwargs)
