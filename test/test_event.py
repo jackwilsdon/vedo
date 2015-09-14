febeef46f74a2c89af68d6f067fb2d48dbda635b
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-09-14 16:33:05 +0100

    Use class test case as base class instead of base event test

--- test/test_event.py
+++ test/test_event.py
@@ -2,44 +2,30 @@ from __future__ import absolute_import
 
 from unittest import TestCase
 
+from vedo.test import ClassTestCase
 from vedo.event_emitter import Event, ReadOnlyEvent
 
-
-class BaseEventTest(TestCase):
-    DEFAULT_EVENT_NAME = 'default event name'
-
+class EventTest(ClassTestCase):
     def setUp(self):
-        self.event_class = Event
+        self.args = ['default event name']
+        self.test_class = Event
 
-    @property
-    def event_class(self):
-        return self._event_class
-
-    @event_class.setter
-    def event_class(self, event_class):
-        self._event_class = event_class
-
-    def create_event(self, name=DEFAULT_EVENT_NAME, *args, **kwargs):
-        return self.event_class(name=name, *args, **kwargs)
-
-
-class EventTest(BaseEventTest):
     def test_name(self):
         event_name = 'event name'
-        event = self.create_event(event_name)
+        event = self.create_test_class(event_name)
 
         self.assertEqual(event.name, event_name)
 
     def test_get(self):
         property_key = 'testing'
         property_value = True
-        event = self.create_event(properties={property_key: property_value})
+        event = self.create_test_class(properties={property_key: property_value})
 
         self.assertEqual(event.get(property_key), property_value)
 
     def test_get_nonexistent(self):
         property_key = 'testing'
-        event = self.create_event()
+        event = self.create_test_class()
 
         with self.assertRaises(KeyError):
             event.get(property_key)
@@ -47,7 +33,7 @@ class EventTest(BaseEventTest):
     def test_get_default(self):
         property_key = 'testing'
         default_value = 'default value'
-        event = self.create_event()
+        event = self.create_test_class()
 
         self.assertEqual(event.get(property_key, default_value), default_value)
 
@@ -55,7 +41,7 @@ class EventTest(BaseEventTest):
         property_key = 'testing'
         property_value = True
         new_property_value = False
-        event = self.create_event(properties={property_key: property_value})
+        event = self.create_test_class(properties={property_key: property_value})
 
         event.set(property_key, new_property_value)
         self.assertEqual(event.get(property_key), new_property_value)
@@ -63,12 +49,12 @@ class EventTest(BaseEventTest):
     def test_set_nonexistent(self):
         property_key = 'testing'
         new_property_value = True
-        event = self.create_event()
+        event = self.create_test_class()
 
         with self.assertRaises(KeyError):
             event.set(property_key, new_property_value)
 
-
+"""
 class ReadOnlyEventTest(EventTest):
     def setUp(self):
         self.event_class = ReadOnlyEvent
@@ -77,7 +63,7 @@ class ReadOnlyEventTest(EventTest):
         property_key = 'testing'
         property_value = True
         new_property_value = False
-        event = self.create_event({property_key: property_value})
+        event = self.create_test_class({property_key: property_value})
 
         with self.assertRaises(RuntimeError):
             event.set(property_key, new_property_value)
@@ -85,7 +71,8 @@ class ReadOnlyEventTest(EventTest):
     def test_set_nonexistent(self):
         property_key = 'testing'
         new_property_value = True
-        event = self.create_event()
+        event = self.create_test_class()
 
         with self.assertRaises(RuntimeError):
             event.set(property_key, new_property_value)
+"""
