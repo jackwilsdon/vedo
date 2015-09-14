b079371fd81b2bf2cc5c7e65c4054b0afbc87865
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-09-14 17:43:17 +0100

    Remove default arguments (too complex)

--- vedo/test/class_test_case.py
+++ vedo/test/class_test_case.py
@@ -3,35 +3,11 @@ from __future__ import absolute_import
 from unittest import TestCase
 from nose.tools import nottest
 
-
-def merge_dicts(base, cover):
-    merged = {}
-
-    for key in base:
-        merged[key] = base[key]
-
-    for key in cover:
-        merged[key] = cover[key]
-
-    return merged
-
-
 class ClassTestCase(TestCase):
-    DEFAULT = object()
-
     def __init__(self, *args, **kwargs):
         super(ClassTestCase, self).__init__(*args, **kwargs)
 
-        self._kwargs = {}
         self._test_class = None
-
-    @property
-    def kwargs(self):
-        return self._kwargs
-
-    @kwargs.setter
-    def kwargs(self, kwargs):
-        self._kwargs = kwargs
 
     @property
     def test_class(self):
@@ -41,10 +17,5 @@ class ClassTestCase(TestCase):
     def test_class(self, test_class):
         self._test_class = test_class
 
-    def create_instance(self, **kwargs):
-        if self.test_class is None:
-            return None
-
-        merged_kwargs = merge_dicts(self.kwargs, kwargs)
-
-        return self.test_class(**merged_kwargs)
+    def create_instance(self, *args, **kwargs):
+        return self.test_class(*args, **kwargs)
