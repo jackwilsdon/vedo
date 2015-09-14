dd353f9cdff155de0a6684ffabd16c79d85bb031
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-09-14 16:01:46 +0100

    Reorder test imports

--- vedo/test/__init__.py
+++ vedo/test/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from vedo.test.value_container import ValueContainer
 from vedo.test.output_recorder import OutputRecorder
+from vedo.test.value_container import ValueContainer
 
-__all__ = ['ValueContainer', 'OutputRecorder']
+__all__ = ['OutputRecorder', 'ValueContainer']
