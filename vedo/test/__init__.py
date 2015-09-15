705d58fcb735bdedba5b92a7b16d95da947a67a6
Jack Wilsdon <jack.wilsdon@gmail.com>
2015-09-16 00:33:27 +0100

    Add method to create bound methods dynamically

--- vedo/test/__init__.py
+++ vedo/test/__init__.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import
 
-from vedo.test.dynamic_methods import create_pass_method
+from vedo.test.dynamic_methods import (create_pass_method,
+                                       create_bound_pass_method)
+
 from vedo.test.output_recorder import OutputRecorder
 from vedo.test.value_container import ValueContainer
 
-__all__ = ['create_pass_method', 'OutputRecorder', 'ValueContainer']
+__all__ = ['create_pass_method', 'create_bound_pass_method', 'OutputRecorder',
+           'ValueContainer']
--- vedo/test/dynamic_methods.py
+++ vedo/test/dynamic_methods.py
@@ -1,6 +1,7 @@
 import compiler
+import types
 
-METHOD_SOURCE='def {name}({args}): pass'
+METHOD_SOURCE = 'def {name}({args}): pass'
 
 
 def generate_method_source(name, argc):
@@ -12,8 +13,13 @@ def create_pass_method(name='pass_method', argc=0):
     context = {}
 
     source = generate_method_source(name, argc)
-    compiled = compile(source, '<generated>', 'exec')
+    compiled = compiler.compile(source, '<generated>', 'exec')
 
     eval(compiled, context)
 
     return context[name]
+
+
+def create_bound_pass_method(name='pass_method', argc=0, binding=object()):
+    method = create_pass_method(name, argc)
+    return types.MethodType(method, binding, binding.__class__)
